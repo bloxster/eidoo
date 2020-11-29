80e10559f7f4445d850ac0fdc0c7bf77272c2184
bloxster <40316187+bloxster@users.noreply.github.com>
2020-11-29 19:21:47 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Guida Completa di Eidoo'
-copyright = u'2014, Read the Docs'
+copyright = u'GNU General License'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -117,7 +117,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = https://imgur.com/AQQDYZH.jpeg
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
